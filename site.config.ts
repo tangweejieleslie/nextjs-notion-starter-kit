22971c3396d44d51a05b6ffe6fec8c871f9aaf4d
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2024-04-14 08:12:47 +0900

    setup default page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '78fade18c36b4dc09f88edce6bb81af2',
+  rootNotionPageId: 'a05118ba4c0744f3b2e227d52d469f78',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
